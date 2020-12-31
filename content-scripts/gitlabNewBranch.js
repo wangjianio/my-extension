6f71a0f4b9ec38a9621742a8fe9447a90f092940
王建 <wangjianio@users.noreply.github.com>
2020-12-31 14:35:03 +0800

    快捷merge按钮

--- content-scripts/gitlabNewBranch.js
+++ content-scripts/gitlabNewBranch.js
@@ -1,9 +1,37 @@
+const authenticity_token = 'gCjAU7PfXBv6RC4K8luTBeaEleIIo6fSx+PhWj1mtIsYgJF4dnkzOC+Ssk4fLrr5U7Dt/u5oz5SJOZvDejXgPg==';
+
 function createNewBranch(branchName, ref = 'master') {
   const formData = new FormData();
   formData.append('utf8', '1');
-  formData.append('authenticity_token', 'gCjAU7PfXBv6RC4K8luTBeaEleIIo6fSx+PhWj1mtIsYgJF4dnkzOC+Ssk4fLrr5U7Dt/u5oz5SJOZvDejXgPg==');
+  formData.append('authenticity_token', authenticity_token);
   formData.append('branch_name', branchName);
   formData.append('ref', ref);
+  fetch('branches', {
+    method: 'POST',
+    body: formData,
+  }).then(() => {
+    location.reload();
+  });
+}
+
+function createMergeRequest(sourceBranch, targetBranch) {
+  const projectId = document.getElementById('search_project_id').value;
+
+  const formData = new FormData();
+  formData.append();
+  formData.append('utf8', '1');
+  formData.append('authenticity_token', authenticity_token);
+  formData.append('merge_request[title]', 'title');
+  // formData.append('merge_request[description]', '');
+  // formData.append('merge_request[assignee_id]', '');
+  // formData.append('merge_request[label_ids]', '');
+  formData.append('merge_request[force_remove_source_branch]', '0');
+  // formData.append('merge_request[lock_version]', '');
+  formData.append('merge_request[source_project_id]', projectId);
+  formData.append('merge_request[source_branch]', '');
+  formData.append('merge_request[target_project_id]', projectId);
+  formData.append('merge_request[target_branch]', '');
+
   fetch('branches', {
     method: 'POST',
     body: formData,
