0d03993b4809e482f5c11625c1311edc2327f399
王建 <wangjianio@users.noreply.github.com>
2021-02-16 20:49:45 +0800

    增加 merge to bus / merge 快捷按钮

--- content-scripts/gitlabNewBranch.js
+++ content-scripts/gitlabNewBranch.js
@@ -21,25 +21,24 @@ function createMergeRequest(sourceBranch, targetBranch) {
   const projectId = document.getElementById('search_project_id').value;
 
   const formData = new FormData();
-  formData.append();
   formData.append('utf8', '1');
   formData.append('authenticity_token', authenticity_token);
-  formData.append('merge_request[title]', 'title');
+  formData.append('merge_request[title]', `${sourceBranch} -> ${targetBranch}`);
   // formData.append('merge_request[description]', '');
   // formData.append('merge_request[assignee_id]', '');
   // formData.append('merge_request[label_ids]', '');
   formData.append('merge_request[force_remove_source_branch]', '0');
   // formData.append('merge_request[lock_version]', '');
   formData.append('merge_request[source_project_id]', projectId);
-  formData.append('merge_request[source_branch]', '');
+  formData.append('merge_request[source_branch]', sourceBranch);
   formData.append('merge_request[target_project_id]', projectId);
-  formData.append('merge_request[target_branch]', '');
+  formData.append('merge_request[target_branch]', targetBranch);
 
-  fetch('branches', {
+  fetch('merge_requests', {
     method: 'POST',
     body: formData,
-  }).then(() => {
-    location.reload();
+  }).then((res) => {
+    location.href = res.url + '/diffs';
   });
 }
 
@@ -51,21 +50,6 @@ if (/\/branches$/.test(location.pathname)) {
       return refName.textContent.trim() === branchName;
     }));
   }
-
-  // const branchListItem = allBranches.querySelectorAll('li');
-  // Array.from(branchListItem).forEach(item => {
-  //   if (/js-/.test(item.className)) {
-  //     const controls = item.querySelector('.controls');
-  //     function appendControlButton(type) {
-  //       const button = document.createElement('a');
-  //       button.setAttribute('class', 'btn btn-default');
-  //       button.textContent = `Merge to ${type}`;
-  //       controls.insertBefore(button, controls.children[0]);
-  //     }
-  //     appendControlButton('merge');
-  //     appendControlButton('bus');
-  //   }
-  // });
 
   const date = new Date();
   const dateString = `${date.getFullYear()}${String(date.getMonth() + 1).padStart(2, '0')}${String(date.getDate()).padStart(2, '0')}`;
@@ -92,6 +76,33 @@ if (/\/branches$/.test(location.pathname)) {
 
   appendNewButton('bus');
   appendNewButton('merge');
+
+  const branchListItem = allBranches.querySelectorAll('li');
+  Array.from(branchListItem).forEach(item => {
+    if (/js-branch/.test(item.className) && item.className !== 'js-branch-master') {
+      const controls = item.querySelector('.controls');
+      function appendControlButton(type) {
+        const button = document.createElement('a');
+        button.setAttribute('class', 'btn btn-default');
+        button.textContent = `Merge to ${type}`;
+        const sourceBranch = item.className.replace(/^js-branch-/, '');
+        const targetBranch = `${type}/${dateString}`;
+        if (isExist(targetBranch)) {
+          if (sourceBranch === targetBranch) {
+            button.style.visibility = 'hidden';
+          }
+          controls.insertBefore(button, controls.children[0]);
+        }
+        button.addEventListener('click', () => {
+          createMergeRequest(sourceBranch, targetBranch);
+          button.setAttribute('disabled', '');
+          button.insertBefore(iconLoading, button.firstChild);
+        });
+      }
+      appendControlButton('merge');
+      appendControlButton('bus');
+    }
+  });
 }
 
 function http2https() {
